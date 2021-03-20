6639f2dfad1bc1cfa326ee991d49d8ca98cf7157
BYRON MIRANDA <libsmirs@gmail.com>
2021-03-20 12:51:30 +0800

    upload upto 03.21.2021

---
+++ IPT.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ipt;
+
+/**
+ *
+ * @author acer
+ */
+
+
+    /**
+     * @param args the command line arguments
+     */
+ 
+       
+public class IPT {
+    
+    public void member2(){
+        int b;
+        System.out.println("I'm Byron L. Miranda");
+        System.out.println("From San Roque, Tarlac City");
+           System.out.println("Part of this group");
+           System.out.println("\n");
+           System.out.println("This is our simple program");
+        
+    
+    }
+  
+    public static void main(String[] args) {
+       IPT objint = new IPT();
+       
+        objint.member2();
+          
+    }
+    
+    }
+    
